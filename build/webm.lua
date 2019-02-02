fee777a8d4fc436040f357e6170042f058851358
ekisu <ramonzitosdesorte@hotmail.com>
2019-02-02 17:18:51 -0200

    revert "util: add get_mpv_path"
    and we've gone full circle: added get_mpv_path because windows needed
    it for display progress, windows is suddenly so broken that display
    progress simply doesn't work, and now even get_mpv_path don't always
    work.
    
    pretty interesting stuff. not.

--- build/webm.lua
+++ build/webm.lua
@@ -152,26 +152,6 @@ local trim
 trim = function(s)
   return s:match("^%s*(.-)%s*$")
 end
-local get_mpv_path
-get_mpv_path = function()
-  if not is_windows then
-    return "mpv"
-  end
-  local pid = utils.getpid()
-  local res = utils.subprocess({
-    args = {
-      "wmic",
-      "process",
-      "where",
-      "processid=" .. tostring(pid),
-      "get",
-      "ExecutablePath",
-      "/VALUE"
-    }
-  })
-  local key_value = trim(res.stdout)
-  return key_value:sub(string.len("ExecutablePath=") + 1)
-end
 local get_null_path
 get_null_path = function()
   if file_exists("/dev/null") then
@@ -1022,7 +1002,7 @@ encode = function(region, startTime, endTime)
   end
   local is_stream = not file_exists(path)
   local command = {
-    get_mpv_path(),
+    "mpv",
     path,
     "--start=" .. seconds_to_time_string(startTime, false, true),
     "--end=" .. seconds_to_time_string(endTime, false, true),
