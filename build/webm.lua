2dc86868d7a6741942a88990a001a21617567e82
ekisu <ramonzitosdesorte@hotmail.com>
2019-03-18 01:51:44 -0300

    options: change output_directory quotes to square brackets
    using quotes caused windows users to accidentally create invalid escape
    sequences (for example, C:\Users contains \U), and those crashed the
    script. using [[ and ]] as delimiters ignores those escape sequences.
    
    also, when your "updates" are literally 6 characters of code and 5 lines
    of comments.
    
    fixes #33

--- build/webm.lua
+++ build/webm.lua
@@ -8,7 +8,12 @@ local options = {
 	keybind = "W",
 	-- If empty, saves on the same directory of the playing video.
 	-- A starting "~" will be replaced by the home dir.
-	output_directory = "",
+	-- This field is delimited by double-square-brackets - [[ and ]] - instead of
+	-- quotes, because Windows users might run into a issue when using
+	-- backslashes as a path separator. Examples of valid inputs for this field
+	-- would be: [[]] (the default, empty value), [[C:\Users\John]] (on Windows),
+	-- and [[/home/john]] (on Unix-like systems eg. Linux).
+	output_directory = [[]],
 	run_detached = false,
 	-- Template string for the output file
 	-- %f - Filename, with extension
