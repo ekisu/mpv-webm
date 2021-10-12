03bda3f5aea283c2817ea64c08206c8f749aa2ab
ekisu <dts.ramon@gmail.com>
2021-10-12 00:54:53 -0300

    tests/run_tests: exit with error code on fail

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,8 +1,11 @@
 import unittest
 from pathlib import Path
+import sys
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
     suite = loader.discover(Path(__file__).parent.absolute())
     runner = unittest.TextTestRunner()
-    runner.run(suite)
+    result = runner.run(suite)
+
+    sys.exit(0 if result.wasSuccessful() else 1)
